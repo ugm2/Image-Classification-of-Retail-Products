fe41900f1d85a7754a4d8d72ea24e28305db345f
ugmSorcero <unai@sorcero.com>
2021-12-29 21:01:48 +0000

    Delete download_dataset.sh

--- download_dataset.sh
+++
@@ -1,3 +0,0 @@
-wget http://aisdatasets.informatik.uni-freiburg.de/freiburg_groceries_dataset/freiburg_groceries_dataset.tar.gz &&
-tar -xvf freiburg_groceries_dataset.tar.gz &&
-rm freiburg_groceries_dataset.tar.gz
